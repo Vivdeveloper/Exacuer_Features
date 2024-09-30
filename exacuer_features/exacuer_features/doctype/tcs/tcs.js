f86b61534f12f7e57041bbacf1c144431ec5affa
Swapnilghadigaonkar <swapnil@hybrowlabs.in>
2024-09-30 13:03:39 +0530

    filter in child

--- exacuer_features/exacuer_features/doctype/tcs/tcs.js
+++ exacuer_features/exacuer_features/doctype/tcs/tcs.js
@@ -1,31 +1,18 @@
-// frappe.ui.form.on('TCS ACCOUNTS', {
-//     company: function(frm, cdt, cdn) {
-//         var row = locals[cdt][cdn];
-//         if (row.company) {
-//             frappe.call({
-//                 method: 'frappe.client.get_list',
-//                 args: {
-//                     doctype: 'Account',
-//                     filters: {
-//                         'company': row.company
-//                     },
-//                     fields: ['name']
-//                 },
-//                 callback: function(data) {
-//                     if (data.message) {
-//                         var accounts = $.map(data.message, function(d) {
-//                             return d.name;
-//                         });
-//                         frm.fields_dict['accounts'].grid.get_field('account').get_query = function() {
-//                             return {
-//                                 filters: {
-//                                     'name': ['in', accounts]
-//                                 }
-//                             };
-//                         };
-//                     }
-//                 }
-//             });
-//         }
-//     }
-// });
+frappe.ui.form.on('TCS', {
+    refresh: function(frm) {
+        frm.fields_dict['accounts'].grid.get_field('account').get_query = function(doc, cdt, cdn) {
+            var child_row = locals[cdt][cdn];
+            if (child_row.company) {
+                return {
+                    filters: {
+                        'company': child_row.company
+                    }
+                };
+            } else {
+                return {
+                    filters: {}
+                };
+            }
+        };
+    }
+});
