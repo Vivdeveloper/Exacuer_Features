06e94f02f9697636229f9a950368eaa444bfb05a
Swapnilghadigaonkar <swapnil@hybrowlabs.in>
2024-09-30 10:44:29 +0530

    filter remove

--- exacuer_features/exacuer_features/doctype/tcs/tcs.js
+++ exacuer_features/exacuer_features/doctype/tcs/tcs.js
@@ -1,31 +1,31 @@
-frappe.ui.form.on('TCS ACCOUNTS', {
-    company: function(frm, cdt, cdn) {
-        var row = locals[cdt][cdn];
-        if (row.company) {
-            frappe.call({
-                method: 'frappe.client.get_list',
-                args: {
-                    doctype: 'Account',
-                    filters: {
-                        'company': row.company
-                    },
-                    fields: ['name']
-                },
-                callback: function(data) {
-                    if (data.message) {
-                        var accounts = $.map(data.message, function(d) {
-                            return d.name;
-                        });
-                        frm.fields_dict['accounts'].grid.get_field('account').get_query = function() {
-                            return {
-                                filters: {
-                                    'name': ['in', accounts]
-                                }
-                            };
-                        };
-                    }
-                }
-            });
-        }
-    }
-});
+// frappe.ui.form.on('TCS ACCOUNTS', {
+//     company: function(frm, cdt, cdn) {
+//         var row = locals[cdt][cdn];
+//         if (row.company) {
+//             frappe.call({
+//                 method: 'frappe.client.get_list',
+//                 args: {
+//                     doctype: 'Account',
+//                     filters: {
+//                         'company': row.company
+//                     },
+//                     fields: ['name']
+//                 },
+//                 callback: function(data) {
+//                     if (data.message) {
+//                         var accounts = $.map(data.message, function(d) {
+//                             return d.name;
+//                         });
+//                         frm.fields_dict['accounts'].grid.get_field('account').get_query = function() {
+//                             return {
+//                                 filters: {
+//                                     'name': ['in', accounts]
+//                                 }
+//                             };
+//                         };
+//                     }
+//                 }
+//             });
+//         }
+//     }
+// });
